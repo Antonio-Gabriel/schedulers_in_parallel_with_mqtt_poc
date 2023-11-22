995cb268f01fc30fc282695619a12dfabe8e6479
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-11-22 10:47:18 +0100

    feat(db): configure db methods

--- src/db/in_momery_db.py
+++ src/db/in_momery_db.py
@@ -6,28 +6,33 @@ from pathlib import Path
 from src.config import ROOT
 from src.models.time_model import TimeModel
 
+from src.constants.out_colors import GREEN, WHITE, ENDC
+
 times: List[TimeModel] = []
 
+
 class InMemoryDB:
-  def __init__(self) -> None:
-    self.__schedulers = []
-    self.__source = Path(f"{ROOT}/db/schedulers.json")
+    def __init__(self) -> None:
+        self.__schedulers = []
+        self.__source = Path(f"{ROOT}/db/schedulers.json")
 
-    if len(json.loads(self.__source.read_text())) > 0:
-      self.__schedulers.append(json.loads(self.__source.read_text())[0])
+        if len(json.loads(self.__source.read_text())) > 0:
+            self.__schedulers.append(json.loads(self.__source.read_text())[0])
 
-  def save_schedulers(self, date: str, times: List[str]):
-    """adding schedulers into file db"""
-    schedule_payload = {
-      "date": date,
-      "times": times
-    }
+    def save_schedulers(self, date: str, times: List[str]):
+        """adding schedulers into file db"""
+        schedule_payload = {
+            "date": date,
+            "times": times
+        }
 
-    self.__schedulers.append(schedule_payload)
-    dump_to_json = json.dumps(self.__schedulers, indent=1)
+        self.__schedulers.append(schedule_payload)
+        dump_to_json = json.dumps(self.__schedulers, indent=1)
 
-    self.__source.write_text(dump_to_json)
-  
-  def get_schedulers(self):
-    """return schedulers from file db"""
-    return self.__schedulers
+        self.__source.write_text(dump_to_json)
+
+        print(f"[{GREEN}DB{ENDC}]: {WHITE}SCHEDULER SAVED SUCCESSFULLY{ENDC}")
+
+    def get_schedulers(self):
+        """return schedulers from file db"""
+        return self.__schedulers
